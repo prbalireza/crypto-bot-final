5b3956a03989a8758b2fa54f6f30e68027d8d0fa
prbalireza <a.pourbabaee@yahoo.com>
2025-04-26 14:56:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 
 from signal_generator import generate_signal
 from telegram_bot import send_signal_to_telegram
-from data_fetcher import get_trending_coins
 
 watchlist = ['BTC/USDT', 'ETH/USDT', 'SOL/USDT', 'XRP/USDT', 'BNB/USDT', 'DOGE/USDT', 'ADA/USDT', 'SHIB/USDT', 'DOT/USDT', 'LTC/USDT']
-watchlist += get_trending_coins()
 
 if __name__ == '__main__':
     for symbol in watchlist:
