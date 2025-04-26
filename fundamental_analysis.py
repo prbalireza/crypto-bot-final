8ee66f9574a9d16059420a377fe6c8f10e10aa72
prbalireza <a.pourbabaee@yahoo.com>
2025-04-26 14:37:49 +0330

    Update fundamental_analysis.py

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -6,13 +6,6 @@ def analyze_fundamentals():
     trending = get_trending_coins()
     sentiment = get_market_sentiment()
     news = get_latest_news()
-
-    # بررسی فقط اخبار با تاثیر مثبت (impact == 'positive')
     positive_news = [n for n in news if n.get('impact') == 'positive']
-
     sentiment_score = len(positive_news) + int(sentiment.get('followers', 0)) // 1000
-
-    return {
-        'trending': trending,
-        'sentiment_score': sentiment_score,
-    }
+    return {'trending': trending, 'sentiment_score': sentiment_score}
