1effa9fe968b3eb43f7e7862f89df33b126478e9
prbalireza <a.pourbabaee@yahoo.com>
2025-04-26 12:38:36 +0330

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,18 +1,17 @@
 # data_fetcher.py
 
-import ccxt
 import requests
-from config import BINANCE_API_KEY, BINANCE_SECRET_KEY, COINMARKETCAP_API_KEY, CRYPTOCOMPARE_API_KEY, CRYPTOPANIC_API_KEY
+from config import COINMARKETCAP_API_KEY, CRYPTOCOMPARE_API_KEY, CRYPTOPANIC_API_KEY
 
-def get_binance_price(symbol='BTC/USDT'):
-    exchange = ccxt.binance({
-        'apiKey': BINANCE_API_KEY,
-        'secret': BINANCE_SECRET_KEY,
-        'enableRateLimit': True,
-    })
-    ticker = exchange.fetch_ticker(symbol)
-    return ticker['last']
+# دریافت قیمت لحظه‌ای از CoinCap
+def get_coincap_price(symbol='bitcoin'):
+    url = f'https://api.coincap.io/v2/assets/{symbol}'
+    response = requests.get(url)
+    data = response.json()
+    price = float(data['data']['priceUsd'])
+    return price
 
+# دریافت کوین‌های ترند از CoinMarketCap
 def get_trending_coins():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/trending/latest'
     headers = {'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY}
@@ -20,12 +19,14 @@ def get_trending_coins():
     data = response.json()
     return [coin['name'] for coin in data['data']]
 
+# احساس بازار از CryptoCompare
 def get_market_sentiment():
     url = f'https://min-api.cryptocompare.com/data/social/coin/latest?api_key={CRYPTOCOMPARE_API_KEY}&fsym=BTC'
     response = requests.get(url)
     data = response.json()
     return data['Data']
 
+# اخبار فاندامنتال از CryptoPanic
 def get_latest_news():
     url = f'https://cryptopanic.com/api/v1/posts/?auth_token={CRYPTOPANIC_API_KEY}&public=true'
     response = requests.get(url)
